2604777c2eba715ef3a7125a8140862ed49b8f33
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2014-05-13 10:14:44 +0200

    new file

---
+++ blueflower.py
@@ -0,0 +1,23 @@
+# copyright (c) 2014 JP Aumasson <jeanphilippe.aumasson@gmail.com>
+#
+# This file is part of blueflower.
+# 
+# blueflower is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# blueflower is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with blueflower.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+if __name__ == '__main__':
+    sys.path.insert(0, '.')  # force local import
+    from blueflower.core import main
+    sys.exit(main())
